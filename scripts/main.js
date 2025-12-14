fb60f1cc78f2e36a8b7efb9e5cc00cd2802a3ca1
csbala <cservenak13alazs@gmail.com>
2025-12-14 14:30:34 +0100

    base

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+/**
+ * Elemental Self Module
+ * Main entry point
+ */
+
+Hooks.once("init", async function () {
+  console.log("Elemental Self | Initializing module");
+});
+
+Hooks.once("ready", async function () {
+  console.log("Elemental Self | Module ready");
+});
